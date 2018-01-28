2737ed9532bae220a940aaf0118a042ad920627c
takato.suzuki <takato_suzuki@suzukitakatonoMacBook-Pro.local>
2018-01-28 14:16:45 +0900

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_many :reviews
+  has_attached_file :avatar, styles: { medium: "300x300#", thumb: "100x100#" }
+  validates_attachment_content_type :avatar, content_type: ["image/jpg","image/jpeg","image/png"]
+  validates :nickname, presence: true  
+end
